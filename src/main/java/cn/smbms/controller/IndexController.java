e54321b4533a078d4090eda035979d216085779b
admin <783585655@qq.com>
2020-10-21 17:24:38 +0800

    first

---
+++ src/main/java/cn/smbms/controller/IndexController.java
@@ -0,0 +1,15 @@
+package cn.smbms.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index.html")
+    public String index(){
+        int num=5/0;
+        return "login";
+    }
+}
